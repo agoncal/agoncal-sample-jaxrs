0944243e6f849cd904726a6b2f734dc903bac691
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-11-30 14:11:07 +0100

    Update 01-Testing/src/test/java/org/agoncal/sample/jaxrs/testing/CustomerRestServiceIT.java
    Changing tests

--- 01-Testing/src/test/java/org/agoncal/sample/jaxrs/testing/CustomerRestServiceIT.java
+++ 01-Testing/src/test/java/org/agoncal/sample/jaxrs/testing/CustomerRestServiceIT.java
@@ -2,13 +2,19 @@ package org.agoncal.sample.jaxrs.testing;
 
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientFactory;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.ext.RuntimeDelegate;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.InetSocketAddress;
 import java.net.URI;
 
@@ -22,10 +28,40 @@ import static org.junit.Assert.assertEquals;
 public class CustomerRestServiceIT {
 
     // ======================================
+    // =             Attributes             =
+    // ======================================
+
+    private static final String XML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><customer><email>jsmith@gmail.com</email><firstName>John</firstName><lastName>Smith</lastName><phoneNumber>1234565</phoneNumber></customer>";
+
+    // ======================================
     // =                 Tests              =
     // ======================================
 
     @Test
+    public void shouldMarshallACustomer() throws JAXBException {
+        // given
+        Customer book = new Customer("John", "Smith", "jsmith@gmail.com", "1234565");
+        StringWriter writer = new StringWriter();
+        JAXBContext context = JAXBContext.newInstance(Customer.class);
+        Marshaller m = context.createMarshaller();
+        m.marshal(book, writer);
+
+        // then
+        assertEquals(XML, writer.toString());
+    }
+
+    @Test
+    public void shouldMarshallAListOfCustomers() throws JAXBException {
+        Customers books = new Customers();
+        books.add(new Customer("John", "Smith", "jsmith@gmail.com", "1234565"));
+        books.add(new Customer("John", "Smith", "jsmith@gmail.com", "1234565"));
+        StringWriter writer = new StringWriter();
+        JAXBContext context = JAXBContext.newInstance(Customers.class);
+        Marshaller m = context.createMarshaller();
+        m.marshal(books, writer);
+    }
+
+    @Test //@Ignore
     public void shouldCheckURIs() throws IOException {
 
         URI uri = UriBuilder.fromUri("http://localhost/").port(8282).build();
@@ -53,6 +89,59 @@ public class CustomerRestServiceIT {
 
         // Stop HTTP server
         server.stop(0);
+    }
 
+    @Test //@Ignore
+    public void shouldGetCustomerByLogin() throws IOException {
+
+        URI uri = UriBuilder.fromUri("http://localhost/").port(8282).build();
+
+        // Create an HTTP server listening at port 8282
+        HttpServer server = HttpServer.create(new InetSocketAddress(uri.getPort()), 0);
+        // Create a handler wrapping the JAX-RS application
+        HttpHandler handler = RuntimeDelegate.getInstance().createEndpoint(new ApplicationConfig(), HttpHandler.class);
+        // Map JAX-RS handler to the server root
+        server.createContext(uri.getPath(), handler);
+        // Start the server
+        server.start();
+
+        Client client = ClientFactory.newClient();
+
+        // Valid URIs
+        Response response= client.target("http://localhost:8282/customer/agoncal").request().get();
+        assertEquals(200, response.getStatus());
+
+        System.out.println("###############################");
+        System.out.println(response.readEntity(String.class));
+
+        // Stop HTTP server
+        server.stop(0);
+    }
+
+    @Test //@Ignore
+    public void shouldGetCustomers() throws IOException {
+
+        URI uri = UriBuilder.fromUri("http://localhost/").port(8282).build();
+
+        // Create an HTTP server listening at port 8282
+        HttpServer server = HttpServer.create(new InetSocketAddress(uri.getPort()), 0);
+        // Create a handler wrapping the JAX-RS application
+        HttpHandler handler = RuntimeDelegate.getInstance().createEndpoint(new ApplicationConfig(), HttpHandler.class);
+        // Map JAX-RS handler to the server root
+        server.createContext(uri.getPath(), handler);
+        // Start the server
+        server.start();
+
+        Client client = ClientFactory.newClient();
+
+        // Valid URIs
+        Response response= client.target("http://localhost:8282/customer").request().get();
+        assertEquals(200, response.getStatus());
+
+        System.out.println("###############################");
+        System.out.println(response.readEntity(String.class));
+
+        // Stop HTTP server
+        server.stop(0);
     }
 }
