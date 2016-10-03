6c47c43339ec0d8fb3af9af4553d9adaf4f8486e
Antonio Goncalves <antonio.goncalves@gmail.com>
2016-10-03 15:41:58 +0200

    Fixing readme

--- 04-JWT/src/main/java/org/agoncal/sample/jaxrs/jwt/util/SimpleKeyGenerator.java
+++ 04-JWT/src/main/java/org/agoncal/sample/jaxrs/jwt/util/SimpleKeyGenerator.java
@@ -17,7 +17,7 @@ public class SimpleKeyGenerator implements KeyGenerator {
 
     @Override
     public Key generateKey() {
-        String keyString = "thisisthepassord";
+        String keyString = "simplekey";
         Key key = new SecretKeySpec(keyString.getBytes(), 0, keyString.getBytes().length, "DES");
         return key;
     }
